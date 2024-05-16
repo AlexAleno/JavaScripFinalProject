fe326e459741b53256c0bbbe8f9319b7e090ab4e
IvascuVictor <ivascu.victor.cegep@gmail.com>
2024-05-16 11:03:45 -0400

    Add files via upload

---
+++ Final-App/src/StockGamer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const StockGamer = () => {
+  return (
+    <div>
+      <iframe
+        title="StockGamer"
+        width="560"
+        height="315"
+        src="GamingStockVideo.mp4"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default StockGamer;
