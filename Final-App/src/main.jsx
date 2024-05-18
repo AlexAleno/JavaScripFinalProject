711b4da441c8f8f7c92b08baf51866a906047f33
AlexAleno <167229685+AlexAleno@users.noreply.github.com>
2024-05-17 22:30:49 -0400

    Update main.jsx
    Added last components

--- Final-App/src/main.jsx
+++ Final-App/src/main.jsx
@@ -5,13 +5,15 @@ import './index.css'
 import Contact from './Contact.jsx'
 import Footer from './Footer.jsx'
 import StockGamer from './StockGamer.jsx'
-
+import DisplayMap from './DisplayMap.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
        <App /> 
       
        <Footer />
+    
      <StockGamer />
+    <DisplayMap/>
     <Contact />
   
   </React.StrictMode>,
